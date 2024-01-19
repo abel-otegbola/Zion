9298c28702f7c22ac69c2835c3a9e195e438383f
abel-otegbola <abel15655@gmail.com>
2024-01-19 13:45:26 +0100

    fix: change links in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,28 +5,29 @@ function Footer() {
         <div className="">
             <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-[30px] py-[30px] md:px-[9%] px-[3%] border border-transparent border-t-gray-700/[0.09] dark:border-t-gray-100/[0.09]">
                 <ul className="w-full">
-                    <h2 className="py-2 font-semibold uppercase">Products</h2>
-                    <li className="flex w-full"><a href="/explore" className="py-[5px] w-full hover:text-green">Projects</a></li>
-                    <li className="flex w-full"><a href="https://sdgs.un.org" className="py-[5px] w-full hover:text-green">Learn</a></li>
-                    <li className="flex w-full"><a href="/dashboard/create" className="py-[5px] w-full hover:text-green">Create</a></li>
+                    <h2 className="py-2 font-semibold uppercase">Books</h2>
+                    <li className="flex w-full"><a href="/books?query=christian" className="py-[5px] w-full hover:text-green">Christian books</a></li>
+                    <li className="flex w-full"><a href="/books?query=academics" className="py-[5px] w-full hover:text-green">Academics</a></li>
+                    <li className="flex w-full"><a href="/books?query=textbooks" className="py-[5px] w-full hover:text-green">Textbooks</a></li>
+                    <li className="flex w-full"><a href="/books?query=past-questions" className="py-[5px] w-full hover:text-green">Past Questions</a></li>
                 </ul>
                 <ul className="w-full">
-                    <h2 className="py-2 font-semibold uppercase">Support</h2>
-                    <li className="flex w-full"><a href="/FAQs" className="py-[5px] w-full hover:text-green">FAQs</a></li>
-                    <li className="flex w-full"><a href="/terms&conditions" className="py-[5px] w-full hover:text-green">Terms of Service</a></li>
-                    <li className="flex w-full"><a href="/privacypolicy" className="py-[5px] w-full hover:text-green">Privacy Policy</a></li>
+                    <h2 className="py-2 font-semibold uppercase">Questions</h2>
+                    <li className="flex w-full"><a href="/FAQs" className="py-[5px] w-full hover:text-green">What is discernment?</a></li>
+                    <li className="flex w-full"><a href="/terms&conditions" className="py-[5px] w-full hover:text-green">How do I grow as a christian?</a></li>
+                    <li className="flex w-full"><a href="/privacypolicy" className="py-[5px] w-full hover:text-green">Service to God, what does it mean?</a></li>
                 </ul>
                 <ul className="w-full">
-                    <h2 className="py-2 font-semibold uppercase">Resources and Blogs</h2>
-                    <li className="flex w-full"><a href="/explore" className="py-[5px] w-full hover:text-green">Project Resources</a></li>
-                    <li className="flex w-full"><a href="https://sdgs.un.org/goals/goal7" className="py-[5px] w-full hover:text-green">SDG goal7 - Access to affordable, reliable, sustainable and modern energy</a></li>
-                    <li className="flex w-full"><a href="https://sdgs.un.org/events/hlpf-2023-side-event-youth-engagement-systemic-transformation-sdg-summit-implementing-global" className="py-[5px] w-full hover:text-green">Youth engagement for systemic transformation at SDG-summit</a></li>
+                    <h2 className="py-2 font-semibold uppercase">Topics</h2>
+                    <li className="flex w-full"><a href="/topics?query=spiritual-knowlegde" className="py-[5px] w-full hover:text-green">Spiritual Knowledge</a></li>
+                    <li className="flex w-full"><a href="/topics?query=salvation" className="py-[5px] w-full hover:text-green">Salvation</a></li>
+                    <li className="flex w-full"><a href="/topics?query=faith" className="py-[5px] w-full hover:text-green">Faith</a></li>
                 </ul>
                 <ul className="w-full">
-                    <h2 className="py-1 text-lg mb-3 font-bold">Ennovate</h2>
-                    <p className="py-1">300 Park Avenue, Lagos, Nigeria</p>
+                    <h2 className="py-1 text-lg mb-3 font-bold">ZION LIBRARY</h2>
+                    <p className="py-1">OAU, Ile-Ife, Osun state, Nigeria</p>
                     <a href="tel:+2347060989331" className="block mt-4 py-1">+2347060989331</a>
-                    <a href="mailto:support@ennovate.com" className="block py-1">Support@ennovate.com</a>
+                    <a href="mailto:support@ennovate.com" className="block py-1">Support@zion-library.com</a>
                 </ul>
             </div>
             <div className="bg-gray-100 dark:bg-purple/[0.03] dark:text-white text-center">
