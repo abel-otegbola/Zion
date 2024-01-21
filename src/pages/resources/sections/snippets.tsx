9b8674a8521a163b613d9c0866c589de072de2a1
abel-otegbola <abel15655@gmail.com>
2024-01-21 23:31:35 +0100

    feat: change featured to snippets

---
+++ src/pages/resources/sections/snippets.tsx
@@ -0,0 +1,8 @@
+export default function Snippets() {
+    return (
+        <div>
+            <h1 className="font-semibold uppercase text-[15px] text-green mb-2">Snippets</h1>
+            <p className="leading-[130%] text-[12px]">Comprises of sermon snippets, bible texts snippets.</p>
+        </div>
+    )
+}
