9b10bbb7d536646427b8aa718b740456ff0a25d6
abel-otegbola <abel15655@gmail.com>
2024-01-24 05:20:39 +0100

    fix: searchbar options for each page

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -1,11 +1,18 @@
 import { FaSearch } from "react-icons/fa";
 
 
-function Searchbar() {
+function Searchbar({ options }: any) {
 
     return (
         <form action="/search" className="w-full text-[12px] gap-2 flex p-[4px] border border-gray-200 dark:border-slate-100/[0.09] rounded">
-            <input className="px-2 bg-transparent flex-1 outline-none border-transparent focus:outline-green/[0.1]" name="search" type="search" placeholder="Search for books, topics, snippets..." />
+            <input className="px-2 bg-transparent flex-1 outline-none border-transparent focus:outline-green/[0.1]" name="search" type="search" placeholder={`Search ${options.map((option: string) => option)}...`} />
+            <select className="px-2 rounded bg-green/[0.1] dark:bg-black focus:outline focus:outline-green/[0.3] outline-offset-1 border border-gray-500/[0.1] py-2">
+                {
+                    options.map((option: string, i:number) => (
+                        <option key={i}>{option}</option>
+                    ))
+                }
+            </select>
             <button className="px-[12px] bg-green rounded hover:bg-green/[0.7] text-white" type="submit" aria-label="Search projects"><FaSearch /></button>
         </form>
     )
